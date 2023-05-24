5fd09e46f80b2a32a3d2dbcf6e1200464ef2bcdb
George Yarr <2553638y@student.gla.ac.uk>
2023-05-24 14:03:47 +0100

    changed index.js to return a download link that is publicly accessible

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,52 @@
-const express = require('express');
-const multer = require('multer');
-const AWS = require('aws-sdk');
-const env = require('dotenv').config();
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const env = require("dotenv").config();
+const fs = require("fs");
 
-const upload = multer({dest: 'uploads/'});
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 
 AWS.config.update({
-    accessKeyId: process.env.aws_access_key,
-    secretAccessKey: process.env.aws_secret_key,
-    region: process.env.aws_region
+  accessKeyId: process.env.aws_access_key,
+  secretAccessKey: process.env.aws_secret_key,
+  region: process.env.aws_region,
 });
 
 const s3 = new AWS.S3();
 
-
-app.post('/upload', upload.single('video'), (req, res) => {
+app.post("/upload", upload.single("video"), (req, res) => {
   const file = req.file;
-  
+
   const params = {
-    Bucket: 'codetestvidbucket', 
-    Key: file.originalname, 
-    Body: fs.createReadStream(file.path)
+    Bucket: "codetestvidbucket",
+    Key: file.originalname,
+    Body: fs.createReadStream(file.path),
   };
 
-  s3.upload(params, function(err, data) {
-    fs.unlinkSync(file.path);
+  s3.upload(params, function (err, data) {
+    
     if (err) {
       return res.status(500).send(err);
     }
-    res.status(200).send(`File uploaded successfully. ${data.Location}`);
+
+    const params = {
+      Bucket: "codetestvidbucket",
+      Key: file.originalname,
+      Expires: 3600, // time after which URL will expire in seconds
+    };
+
+    s3.getSignedUrl("getObject", params, function (err, url) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      res.status(200).send({ message: "File uploaded successfully", url: url });
+    });
   });
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
